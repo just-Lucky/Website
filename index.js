5133909d0d0bd2a1115dbcd31aeaf266fa8f0b4a
justLucky <rn10p.luckytv@gmail.com>
2024-11-21 19:58:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function changeto(newlang){
         document.getElementById("fst-sec-ttl").textContent = "Benvenuto/a";
         document.getElementById("fst-sec-txt").textContent = "Benvenuto/a sul mio Sito! Sono conosciuto online come 'justLucky'. Esplora il mio Sito per scoprire di più su di me e sulle mie esperienze.";
         document.getElementById("snd-sec-ttl").textContent = "Su di Me:";
-        document.getElementById("snd-sec-txt").textContent = "Ciao! Il mio nickname è 'justLucky'. Sono un ragazzo Italiano con più di 2 anni di esperienza nella Programmazione in Python e in futuro intendo imparare C e Java, inoltre, un giorno spero di diventare un Ethical Hacker di successo. Per commissioni su pagamento che richiedono la Programmazione in Python 3.10 o superiore, sentitevi liberi di contattarmi su Telegram o su Discord, sono aperto alla maggior parte delle richieste.";
+        document.getElementById("snd-sec-txt").textContent = "Ciao! Il mio nickname è 'justLucky'. Sono un ragazzo Italiano che studia attivamente la programmazione in Python dall'Agosto 2022 e in futuro intendo imparare C e Java, inoltre, un giorno spero di diventare un Ethical Hacker di successo. Per commissioni su pagamento che richiedono la Programmazione in Python 3.10 o superiore, sentitevi liberi di contattarmi su Telegram o su Discord, sono aperto alla maggior parte delle richieste.";
         document.getElementById("trd-sec-ttl").textContent = "Come contattarmi:";
         document.getElementById("trd-sec-txt").textContent = "Se desiderate contattarmi, ecco come:";
         document.getElementById("trd-sec-sndtxt").innerHTML = 'Email: <a href="mailto:business1412riccardob@gmail.com">business1412riccardob@gmail.com</a> (La controllo raramente)';
@@ -96,7 +96,7 @@ function changeto(newlang){
         document.getElementById("fst-sec-ttl").textContent = "Welcome";
         document.getElementById("fst-sec-txt").textContent = "Welcome to my Website! I'm known online as 'justLucky'. Explore my site to learn more about me and my work.";
         document.getElementById("snd-sec-ttl").textContent = "About Me";
-        document.getElementById("snd-sec-txt").textContent = "Hi! My nickname is justLucky. I'm an Italian guy with more than 2 years of experience in Python programming and I want to learn C and Java in the future, also, one day I hope to become a successful Ethical Hacker. For paid commissions that require programming in Python 3.10 or higher, feel free to contact me on Telegram or Discord, I'm open to most requests.";
+        document.getElementById("snd-sec-txt").textContent = "Hi! My nickname is justLucky. I'm an Italian guy that has been actively learning and programming in Python since August 2022 and I want to learn C and Java in the future, also, one day I hope to become a successful Ethical Hacker. For paid commissions that require programming in Python 3.10 or higher, feel free to contact me on Telegram or Discord, I'm open to most requests.";
         document.getElementById("trd-sec-ttl").textContent = "Contact Me";
         document.getElementById("trd-sec-txt").textContent = "If you want to get in touch with me, here are my contact details:";
         document.getElementById("trd-sec-sndtxt").innerHTML = 'Email: <a href="mailto:business1412riccardob@gmail.com">business1412riccardob@gmail.com</a> (I rarely check it)';
