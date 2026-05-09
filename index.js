597c15947f941658634431e535f8b6a2805cf9d9
justLucky <rn10p.luckytv@gmail.com>
2026-05-09 19:39:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,12 +75,11 @@ function changeto(newlang){
         document.getElementById("lang-en").textContent = "Inglese";
         document.getElementById("lang-it").textContent = "Italiano";
         document.getElementById("fst-sec-ttl").textContent = "Benvenuto/a";
-        document.getElementById("fst-sec-txt").textContent = "Benvenuto/a sul mio Sito! Sono conosciuto online come 'justLucky'. Esplora il mio Sito per scoprire di più su di me e sulle mie esperienze.";
+        document.getElementById("fst-sec-txt").textContent = "Benvenuto/a sul mio Sito! Sono conosciuto online come 'justLucky'. Esplora il mio Sito per scoprire di più su di me e sulla mia vita.";
         document.getElementById("snd-sec-ttl").textContent = "Su di Me:";
-        document.getElementById("snd-sec-txt").textContent = "Ciao! Il mio nickname è 'justLucky', ma il mio vero nome è Riccardo. Sono un ragazzo Italiano che studia attivamente la programmazione in Python dall'Agosto 2022 e in futuro intendo imparare C e Java, inoltre, un giorno spero di diventare un Ethical Hacker di successo. Per commissioni su pagamento che richiedono la Programmazione in Python 3.10 o superiore, sentitevi liberi di contattarmi su Telegram o su Discord, sono aperto alla maggior parte delle richieste.";
+        document.getElementById("snd-sec-txt").textContent = "Ciao! Il mio nickname è 'justLucky', ma il mio vero nome è Riccardo, sono Cattolico e amo Gesù; il mio passo preferito nella Bibbia è Matteo 6:25-34! Studio attivamente la programmazione in Python dall'Agosto 2022, conosco le basi di C/C++, Java e Javascript. Adoro anche le reti e la cybersicurezza. Per commissioni su pagamento che richiedono la Programmazione in Python 3.10 o superiore, sentitevi liberi di contattarmi su Telegram o su Instagram, potete anche contattarmi sulla mia Email, ma non la controllo spesso. Adoro giocare a scacchi, anche se non sono molto bravo, le mie aperture preferite sono il Sistema Londinese e la Difesa Caro-Kann.";
         document.getElementById("trd-sec-ttl").textContent = "Come contattarmi:";
         document.getElementById("trd-sec-txt").textContent = "Se desiderate contattarmi, ecco come:";
-        document.getElementById("trd-sec-sndtxt").innerHTML = 'Email: <a href="mailto:business1412riccardob@gmail.com">business1412riccardob@gmail.com</a>';
         document.getElementById("footer-txt").innerHTML = "Sito di justLucky"
     }
     else if(newlang === 'en'){
@@ -89,12 +88,11 @@ function changeto(newlang){
         document.getElementById("lang-en").textContent = "English";
         document.getElementById("lang-it").textContent = "Italian";
         document.getElementById("fst-sec-ttl").textContent = "Welcome";
-        document.getElementById("fst-sec-txt").textContent = "Welcome to my Website! I'm known online as 'justLucky'. Explore my site to learn more about me and my work.";
+        document.getElementById("fst-sec-txt").textContent = "Welcome to my Website! I'm known online as 'justLucky'. Explore my site to learn more about me and my life.";
         document.getElementById("snd-sec-ttl").textContent = "About Me";
-        document.getElementById("snd-sec-txt").textContent = "Hi! My nickname is justLucky, but my real name is Riccardo. I'm an Italian guy that has been actively learning and programming in Python since August 2022 and I want to learn C and Java in the future, also, one day I hope to become a successful Ethical Hacker. For paid commissions that require programming in Python 3.10 or higher, feel free to contact me on Telegram or Discord, I'm open to most requests.";
+        document.getElementById("snd-sec-txt").textContent = "Hi! My nickname is justLucky, but my real name is Riccardo, I'm a Roman Catholic and I love Jesus; my favorite passage in the Bible is Matthew 6:25-34! I have been actively learning and programming in Python since August 2022, I know the basics of C/C++, Java and Javascript. I also love networking and cybersecurity. For paid commissions that require programming in Python 3.10 or higher, feel free to contact me on Telegram or Instagram, you can also contact me on my Email, but I don't check it often. I love playing chess, although I'm not that good, my favorite openings are the London System and the Caro-Kann Defense.";
         document.getElementById("trd-sec-ttl").textContent = "Contact Me";
         document.getElementById("trd-sec-txt").textContent = "If you want to get in touch with me, here are my contact details:";
-        document.getElementById("trd-sec-sndtxt").innerHTML = 'Email: <a href="mailto:business1412riccardob@gmail.com">business1412riccardob@gmail.com</a>';
         document.getElementById("footer-txt").innerHTML = "justLucky's Website"
     }
 }
